bd9264edfccb994d4682bb80a921d4ebdc6f2018
Tanjinul Hoque <68518881+Tanjinul@users.noreply.github.com>
2023-04-01 12:28:03 -0400

    Completed Task where it submits to a Database

--- About Us Page/about.js
+++ About Us Page/about.js
@@ -11,22 +11,17 @@ function fixNav(){
 }
 
 //Mailing List
-const labels = document.querySelectorAll('.form-control label')
-
-// labels.forEach(label =>{
-//     label.innerHTML = label.innerText
-//         .split('')
-//         .map((letter, idx) =>`<span>${letter}</span>`)
-//         .join('')
-// })
 
 document.addEventListener("DOMContentLoaded", function() {
-   labels.forEach(label => {
+  const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
-        .map((letter, idx) =>`<span style="transition-delay:${idx*50}ms">${letter}</span>`)
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join('')
 })
 
-    
-  });
+
+  
+});
