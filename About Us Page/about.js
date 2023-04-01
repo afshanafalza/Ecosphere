8381c25809079b17499f6f819b995749acd9e10f
mushi20 <107772612+mushi20@users.noreply.github.com>
2023-03-31 23:42:52 -0400

    Add files via upload

---
+++ About Us Page/about.js
@@ -0,0 +1,32 @@
+// taken from "50 Projects in 50 Days" "Sticky Nav Bar"
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll',fixNav)
+
+function fixNav(){
+  if(window.scrollY > nav.offsetHeight + 150){
+    nav.classList.add('active')
+  } else {
+    nav.classList.remove('active')
+  }
+}
+
+//Mailing List
+const labels = document.querySelectorAll('.form-control label')
+
+// labels.forEach(label =>{
+//     label.innerHTML = label.innerText
+//         .split('')
+//         .map((letter, idx) =>`<span>${letter}</span>`)
+//         .join('')
+// })
+
+document.addEventListener("DOMContentLoaded", function() {
+   labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) =>`<span style="transition-delay:${idx*50}ms">${letter}</span>`)
+        .join('')
+})
+
+    
+  });
