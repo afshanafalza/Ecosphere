50db9ba0a0243480ced483389b696cd4b4cc83d5
Maroosha Ishtiaq <107772612+mushi20@users.noreply.github.com>
2023-04-04 00:12:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ app.set('view engine','ejs')
 app.listen(3000, () => {
     console.log('Server listening on port 3000...')
 })
+
+app.get("/about", function (request, response) {
+    response.sendFile(__dirname + './public/About Us Page/about.html');
+});
+
+app.get("/cartindex", function (request, response) {
+    response.sendFile(__dirname + './public/Cart Page/cartindex.html');
+});
+
+app.get("/shopindex", function (request, response) {
+    response.sendFile(__dirname + './public/Shopping Page/shopindex.html');
+});
+
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + './public/index.html');
+});
